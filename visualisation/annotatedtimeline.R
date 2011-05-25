b847986c1e0ef6a49565e54459ea9dee1a9ac1ab
Aartika Rai <aartika.iitd@gmail.com>
2011-05-25 15:26:35 +0530

    added file to visualisation

---
+++ visualisation/annotatedtimeline.R
@@ -0,0 +1,28 @@
+annotatedtimeline <-function(numdays,instr)
+{
+  nDays <- numdays
+  instrument <- instr
+  
+  start <- strftime(as.POSIXlt(Sys.time() - nDays*24*3600), format="%Y-%m-%d") 
+  end <- strftime(as.POSIXlt(Sys.time()), format = "%Y-%m-%d")  
+  x <- get.hist.quote(instrument = instrument, start = start, end = end) 
+  x<- ts(x)
+  frame <- data.frame(attr(x,"index"),x)
+  names(frame)[1]<-"Date"
+  names(frame)[2:5]<-"value"
+  open<- data.frame(frame[1],frame[2], title = "Open")
+  high<- data.frame(frame[1],frame[3], title = "High")
+  low<- data.frame(frame[1],frame[4], title = "Low")
+  close<-data.frame(frame[1],frame[5], title = "Close")
+  bind <-rbind(open, high, low,close)
+AnnoTimeLine  <- gvisAnnotatedTimeLine(bind, datevar="Date",
+                           numvar="value", idvar="title",
+                           options=list(displayAnnotations=TRUE,
+                             legendPosition='newRow',
+                             width=800, height=600)
+                           )
+# Display chart
+plot(AnnoTimeLine) 
+# Create Google Gadget
+cat(createGoogleGadget(AnnoTimeLine), file="annotimeline.xml")
+}
