f7465dcf7b9a3a246d9690eddde0d7cc1abbcc5a
Tarun Jain <tarunjain15@gmail.com>
2011-05-30 15:58:56 +0800

    link.R, machines,parsing.R are added

---
+++ parsing.R
@@ -0,0 +1,6 @@
+doc = xmlRoot(xmlTreeParse("machines"))
+tmp = xmlSApply(doc , function(x) xmlSApply(x, xmlValue))
+tmp = t(tmp)
+machines = as.data.frame(matrix((tmp), 6))
+names(machines) = names(doc[[1]])
+
