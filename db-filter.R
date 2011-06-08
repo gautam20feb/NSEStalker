372d0dbab1371ca77423918f2ce9b90b2d5cf86a
Aartika Rai <aartika.iitd@gmail.com>
2011-06-08 17:58:58 +0530

    modified db-filter

--- db-filter.R
+++ db-filter.R
@@ -1,8 +1,6 @@
 filterEQ <- function(con,reftable,eqtable)
 {
   data <- dbGetQuery(con, paste("select distinct SYMBOL from", reftable, "group by SYMBOL"))
-  print(data)
-  print(list)
   print(paste("delete from", eqtable, "where SYMBOL not in (",paste("'",data[[1]],"'", collapse = ", ",sep = ""), ")"))
   dbGetQuery(con,paste("delete from", eqtable, "where SYMBOL not in (",paste("'",data[[1]],"'", collapse = ", ",sep = ""), ")"))
     
