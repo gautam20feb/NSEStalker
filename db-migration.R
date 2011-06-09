518741606a8516d239081f141676ed9ad1f521ff
Aartika Rai <aartika.iitd@gmail.com>
2011-06-09 16:47:38 +0530

    added comments to db-migration

--- db-migration.R
+++ db-migration.R
@@ -1,6 +1,11 @@
 library(RMySQL)
 m<- dbDriver("MySQL")
-dbtocsv <- function(dbname, folder = getwd())
+dbtocsv <- function
+### the function returns the tables in the given database in the form of csv files, one for each table in the given folder.
+(dbname,
+### name of the database to be read to csv files
+folder = getwd())
+### folder to which the database is to be copied in the form of csv files
 {
   con<-dbConnect(m, dbname = dbname, user = "root", password = "intern123", host = "localhost")
   tables <- dbListTables(con)
@@ -11,7 +16,12 @@ dbtocsv <- function(dbname, folder = getwd())
   }
 }
 
-csvtodb <- function(folder,dbname)
+csvtodb <- function
+### the functions takes all the csv files in the given folder and stores them as different tables in the given database.
+(folder,
+### folder which contains the csv files to be moved to database
+dbname)
+### name of  the database to which the files are to  be moved
 {
     con<-dbConnect(m, dbname = dbname, user = "root", password = "intern123", host = "localhost")
     files <- dir()
