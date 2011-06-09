524afecda1effa97b6b481b01a2e123467c2d6b3
Aartika Rai <aartika.iitd@gmail.com>
2011-06-09 16:50:32 +0530

    added comments to db-filter

--- db-filter.R
+++ db-filter.R
@@ -1,4 +1,11 @@
-filterEQ <- function(con,reftable,eqtable)
+filterEQ<- function
+### removes data corresponding to all those stocks which do not have futures.
+(con,
+## the connection pointing to the mysql database
+reftable,
+### the reference future tablename from the same database
+eqtable)
+### name of the equity table to be filtered
 {
   data <- dbGetQuery(con, paste("select distinct SYMBOL from", reftable, "group by SYMBOL"))
   print(paste("delete from", eqtable, "where SYMBOL not in (",paste("'",data[[1]],"'", collapse = ", ",sep = ""), ")"))
