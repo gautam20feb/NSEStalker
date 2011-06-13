a59415ad9ceaa1c7e983ca414c416fd31e442b5d
Rajesh Kella <krajesh1990@gmail.com>
2011-06-13 18:07:22 +0530

    updated OHLC.R

--- validation/OHLC.R
+++ validation/OHLC.R
@@ -1,50 +1,78 @@
+setwd("~/projects/NSEStalker/validation")
+
 library(RMySQL)
 m<- dbDriver("MySQL")
+### Connection to TWS Database
 conn<-dbConnect(m, dbname = "TWS_daily_data", user = "root", password = "intern123", host = "localhost")
+### Connection to NSEDB
 conn1<-dbConnect(m, dbname = "nsedb", user = "root", password = "intern123", host = "localhost")
 
-mylog2 <- file("Validation_log.csv", "w")  ##<< For logging the files written to database
+### File connections for logging "Uncommon Dates" and "Tracking Status"
+datelog<-file("Diff_date_log.csv","w")
+mylog2 <- file("Stk_OHLC_Validation_log.csv", "w")  ##<< For logging the files written to database
 
+### Initialization of vectors
 open_diff<-c()
 high_diff<-c()
 low_diff<-c()
 close_diff<-c()
 comp<-c()
+common_dates<-c()
+diff<-c()
 
-companies<-dbListTables(conn)
+### List of Companies(from TWS databse)
+nse_companies<-dbGetQuery(conn,"SELECT `NSE_NAME` FROM Stocks")
+tws_companies<-dbGetQuery(conn,"SELECT `NAME_USED` FROM Stocks")
+### List of Companies(from NSE databse)
+nsedb_companies<-dbGetQuery(conn1,"SELECT DISTINCT `SYMBOL`FROM equity")
+### List of common companies from two databases
+common_companies<-intersect(nse_companies[,1],nsedb_companies[,1])
 
-for(j in 1:length(companies))
-{
-query<-paste("SELECT TIMESTAMP FROM `",companies[j],"`",sep="")
-date_TWS<-dbGetQuery(conn,query)
+### Check for companies mismatch
+if (nrow(nse_companies) != length(common_companies)) print("Error : List of Companies from IB is not the same as List of comapnies from NSEDB")
 
-query<-paste("SELECT `TIMESTAMP` FROM `equity`WHERE `SYMBOL` = '",companies[j],"'",sep="")
-date_nsedb<-dbGetQuery(conn1,query)
-
-common_dates<-intersect(date_TWS[,1],date_nsedb[,1])
-diff<-setdiff(date_TWS[,1],date_nsedb[,1])
-
-for(i in 1:length(common_dates))
-{
-  query<-paste("SELECT * FROM `equity`WHERE `TIMESTAMP` = '",common_dates[i],"' AND `SYMBOL` = '",companies[j],"'",sep="")
-  open_nsedb<-dbGetQuery(conn1,query)
-  
-  query<-paste("SELECT * FROM `",companies[j],"` WHERE `TIMESTAMP`='",common_dates[i],"'",sep="")
-  open_TWS<-dbGetQuery(conn,query)
-  
-  open_diff[i]<-open_nsedb[1,4]-open_TWS[1,3]
-  high_diff[i]<-open_nsedb[1,5]-open_TWS[1,4]
-  low_diff[i]<-open_nsedb[1,6]-open_TWS[1,5]
-  close_diff[i]<-open_nsedb[1,7]-open_TWS[1,6]
-  comp[i]<-companies[j]
-  
-  cat(as.character(timestamp()),"Date",common_dates[i],"Stock",as.character(j),companies[j],"\n",file = mylog2, sep = ",")
-
-  
-}
-
-all<-cbind(comp,common_dates,open_diff,high_diff,low_diff,close_diff)
-write.table(all, file="validation.csv", append=TRUE, sep=",")
+ for(j in 1:length(common_companies))
+ {
+   ### Vector of dates for which data is available for a given company from TWS databse
+   query<-paste("SELECT TIMESTAMP FROM `",tws_companies[j,1],"`",sep="")
+   date_TWS<-dbGetQuery(conn,query)
+   ### Vector of dates for which data is available for a given company from NSE databse
+   query<-paste("SELECT `TIMESTAMP` FROM `equity`WHERE `SYMBOL` = '",nse_companies[j,1],"'",sep="")
+   date_nsedb<-dbGetQuery(conn1,query)
+   ### Intersection of two date vectors
+   common_dates<-intersect(date_TWS[,1],date_nsedb[,1])
+   diff<-setdiff(date_TWS[,1],date_nsedb[,1])
+   ### Logging for Dates Mismatch in the two databses
+   for(k in 1: length(diff))
+   {
+     if(length(which(date_TWS==diff[k]))==0) remark<-"Present in NSEDB but not present in TWS"
+     if(length(which(date_nsedb==diff[k]))==0) remark<-"Present in TWS but not present in NSEDB"
+     cat(as.character(j),"Company:",common_companies[j],"Date:",diff[k],"Remark:",remark,"\n",file = datelog, sep = ",")
+   }
+   
+   for(i in 1:length(common_dates))
+   {
+     ### OHLC data from NSEDB
+     query<-paste("SELECT * FROM `equity`WHERE `TIMESTAMP` = '",common_dates[i],"' AND `SYMBOL` = '",nse_companies[j,1],"'",sep="")
+     t1<-system.time(open_nsedb<-dbGetQuery(conn1,query))
+     
+     ### OHLC data from TWSDB
+     query<-paste("SELECT * FROM `",tws_companies[j,1],"` WHERE `TIMESTAMP`='",common_dates[i],"'",sep="")
+     t2<-system.time(open_TWS<-dbGetQuery(conn,query))
+     ### Differnce of two values (Validation)
+     open_diff[i]<-open_nsedb[1,4]-open_TWS[1,3]
+     high_diff[i]<-open_nsedb[1,5]-open_TWS[1,4]
+     low_diff[i]<-open_nsedb[1,6]-open_TWS[1,5]
+     close_diff[i]<-open_nsedb[1,7]-open_TWS[1,6]
+     comp[i]<-common_companies[j]
+     
+     ### Logging for tracking status
+     cat(as.character(timestamp()),"Date",common_dates[i],"Stock",as.character(j),common_companies[j],"CPU Time for SQL Query","T1",t1,"T2",t2,"\n",file = mylog2, sep = ",")
+     
+   }
+   ### Writing output of validation to csv
+   all<-cbind(comp,common_dates,open_diff,high_diff,low_diff,close_diff)
+   write.table(all, file="validation.csv", append=TRUE, sep=",")
 }
 
 dbDisconnect(conn)
