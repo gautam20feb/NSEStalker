16086e6f5a6187fd9a8e95cf5687f8f19a1f866e
Gautam Garg <gautam20feb@gmail.com>
2011-06-16 18:10:30 +0800

    create_db.R- modified

--- db_manage/create_dbs.R
+++ db_manage/create_dbs.R
@@ -1,10 +1,25 @@
 library(RMySQL)
 db<-read.csv("./data/Databases.csv", sep=",", header=F) ### Read the Stock Nam
-n <- dbDriver("MySQL", max.con = 100)
-con <- dbConnect(n, user="gautam", password = "intern123", host = "localhost")
-sapply(db[,2],create.db)
-    
-create.db<-function(x)
+m<- dbDriver("MySQL", max.con = 1000)
+doc = xmlRoot(xmlTreeParse("./config/machines.xml")) ##<< parses all of the config file
+tmp = xmlSApply(doc , function(x) xmlSApply(x, xmlValue)) ##<< creates a matrix of machines information
+tmp = t(tmp) ##<< takes transpose 
+n<-nrow(tmp) ##<< get the number of users
+machines = as.data.frame(matrix((tmp), n)) ##<< produces a dataframe for the matrix
+names(machines) = names(doc[[1]]) ##<< names the corresponding columns
+### Function to create the Databases as mentioned in Databases.csv
+create.db<-function(
+machine.name
+### In the format Usernmae@machine
+)
 {
-  try(dbGetQuery(con,paste("create database ",x,sep="")),silent=TRUE) ## implements the query
+  usr<-unlist(strsplit(machine.name,"@"))[1]
+  pos=which(machines[,1]==machine.name)
+  hst=as.character(machines[pos,2])
+  pswd=as.character(machines[pos,3])
+  con <- dbConnect(m, user=usr, password = pswd, host = hst)
+  for(i in 1 : nrow(db))
+    {
+      try(dbGetQuery(con,paste("create database ",db[i,2],sep="")),silent=TRUE) ## implements the query
+    }  
 }
