19baa2f72e03e9c57d2f094914fa531189d49e5e
Gautam Garg <gautam20feb@gmail.com>
2011-06-21 14:53:25 +0800

    db-filter.R - modified

--- db/db-filter.R
+++ db/db-filter.R
@@ -10,7 +10,7 @@ eqtable="equity"
 )
 ### name of the equity table to be filtered
 {
-  futures_list<-trim(read.csv("./data/futures_list.csv", header=F)[,1])
+  futures_list<-trim(read.csv("./data/futures_list.csv", header=T)[,1])
   query<-paste("delete from", eqtable, "where SYMBOL not in (",paste("'",futures_list,"'", collapse = ", ",sep = ""), ")")
   dbGetQuery(con,query)
   query1<-paste("delete from", reftable, "where SYMBOL not in (",paste("'",futures_list,"'", collapse = ", ",sep = ""), ")")
