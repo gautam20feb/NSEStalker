8ac8ec0769b0fa9bcad2cba801aec11fd08d2e6b
Gautam Garg <gautam20feb@gmail.com>
2011-06-23 22:13:36 +0800

    new files added

---
+++ time_validation/VALIDATION_TWS_OHLC_stocks.R
@@ -0,0 +1,91 @@
+library(timeDate)
+library(fBasics)
+library(fImport)
+library (bitops)
+library (methods)
+library(RMySQL)
+library(XML)
+library(xts)
+library(timeDate)
+
+source("../lib/read_machines.R")
+source("../lib/create_connection.R")
+connection.log <- file(paste("../log/" ,as.character(timestamp()),"log.csv",sep="") , "w")  ##<< For logging the files written to database
+
+config_path <- "../config/"
+
+### This Script Needs futures_list.csv and output.csv file in Data folder
+### It produces for each stock in the database TWS_OHLC_stocks a table having the information about breaks
+### There are 2 tables TIME_LINE & BREAK_POINTS
+### TIME_LINE tells about the start date , end date and the existence of any break
+### BREAK_POINTS gives the breaks if there are any
+TwsOhlcStocksValidateForTime <- function(
+user.name = "intern@Ophelia"
+) {
+  futures_list<-trim(read.csv("../data/futures_list.csv", sep=",", header=T)) ##<< Read the Stock Name
+  all.days<-read.csv<-read.csv("../data/output.csv", sep=",", header=T)
+  days<- all.days[grep("Working Day", all.days$For.Trading, ignore.case=T),]
+  days<-as.character(days[,2])
+
+  no<-nrow(futures_list)
+  
+  machines=GenerateMachimesDataframe(config_path)
+  connection.TWS_OHLC_stocks <- CreateConnection(user.name , machines ,"TWS_OHLC_stocks")
+  connection.VALIDATION_TWS_OHLC_stocks <- CreateConnection(user.name , machines ,"VALIDATION_TWS_OHLC_stocks")
+
+
+
+
+  for(i in 1 :no)
+  {
+    try( if(1==1)
+    {
+    
+    stock=as.character(futures_list[i,4])
+    query<-paste("SELECT DISTINCT TIMESTAMP FROM " , stock," ORDER by TIMESTAMP ASC" ,sep="")
+    tab<-(dbGetQuery(connection.TWS_OHLC_stocks,query)) ## implements the query
+    cat("TWS_OHLC_stocks" ,timestamp(),"TwsOhlcStocksValidateForTime",paste("Read", stock ,sep=" "),"\n",file = connection.log, sep = ",")
+    print(stock)
+    dates<-(tab[1])
+    dates.vector<-as.vector(dates[,1])
+    Start.date<-dates.vector[1]
+    End.date<-tail(dates.vector, n=1L)
+    Start.ind<- which(days==Start.date)
+    End.ind<- which(days==End.date)
+    working.days<- days[Start.ind:End.ind]
+    a<-timeSeries(dates.vector)
+    b<-timeSeries(working.days)
+    difference<-setdiff(b,a)
+    flag<-ifelse(length(difference)>0,1 ,0)
+    
+    tablename="TIME_LINE"
+    d<-cbind(stock,Start.date,End.date,flag)
+    data<-as.data.frame(d)
+    names(data)<-c("SYMBOL","START_DATE","END_DATE","IF_BREAK")
+    str<-ifelse( nrow(data)>0,"Added" , "Not Added")
+    ifelse(dbExistsTable(connection.VALIDATION_TWS_OHLC_stocks, tablename),dbWriteTable(connection.VALIDATION_TWS_OHLC_stocks, name =tablename, value=data, append = T),dbWriteTable(connection.VALIDATION_TWS_OHLC_stocks, name = tablename, value=data))
+    
+    print(nrow(data))
+    if (flag > 0)
+    {
+     tablename="BREAK_POINT"
+     d<-cbind(stock,Start.date,End.date,difference) 
+     data<-as.data.frame(d)
+     names(data)<-c("SYMBOL","START_DATE","END_DATE","BREAK_POINTS")
+     str<-ifelse( 
+     nrow(data)>0
+     ,
+     "Added" , "Not Added")
+     cat("VALIDATION_TWS_OHLC_stocks" ,timestamp(),"TwsOhlcStocksValidateForTime",paste(str, tablename ,sep=" "),"\n",file = connection.log, sep = ",")
+     ifelse(dbExistsTable(connection.VALIDATION_TWS_OHLC_stocks, tablename),dbWriteTable(connection.VALIDATION_TWS_OHLC_stocks, name =tablename, value=data, append = T),dbWriteTable(connection.VALIDATION_TWS_OHLC_stocks, name = tablename, value=data))
+     print(nrow(data))
+    }
+    
+    
+    } 
+  ,silent=TRUE)
+  }
+close(connection.log)
+dbDisconnect(connection.TWS_OHLC_stocks)
+dbDisconnect(connection.VALIDATION_TWS_OHLC_stocks)
+}
---
+++ time_validation/per_min_stocks_time_valid.R
@@ -0,0 +1,73 @@
+library(timeDate)
+library(fBasics)
+library(fImport)
+library (bitops)
+library (methods)
+library(RMySQL)
+library(XML)
+library(xts)
+library(timeDate)
+library(gregmisc)
+
+source("../lib/read_machines.R")
+source("../lib/create_connection.R")
+connection.log <- file(paste("../log/" ,as.character(timestamp()),"log.csv",sep="") , "w")  ##<< For logging the files written to database
+
+config_path <- "../config/"
+
+### This Script Needs stocks_list.csv file in Data folder
+### It produces for each stock in the database TWS_per_min_stocks a table having the information about breaks
+### Each table has columns : "START_DATE","START_TIME","END_DATE","END_TIME","DIFFERENCE","BREAK_TYPE"
+### START_DATE , START_TIME indicate the date,time of start of the interval where there is a break
+### START_DATE , START_TIME indicate the date,time of end of the interval where there is a break
+### DIFFERENCE gives the length ( End - Start ) in minutes
+### BREAK_TYPE indicates the type of the break
+### BREAK_TYPE = 0 if the break is due to the end of the day (Natural break)
+### BREAK_TYPE = 1 if the break is due to some irregularity in data (Irregular break)
+TwsPerminStocksValidateForTime <- function(
+user.name = "intern@Ophelia"
+) {
+  futures_list<-trim(read.csv("../data/futures_list.csv", sep=",", header=T)) ### Read the Stock Name
+  no<-nrow(futures_list)
+
+  machines=GenerateMachimesDataframe(config_path)
+  connection.TWS_PERMIN_stocks <- CreateConnection(user.name , machines ,"TWS_PERMIN_stocks")
+  connection.VALIDATION_TWS_PERMIN_stocks <- CreateConnection(user.name , machines ,"VALIDATION_TWS_PERMIN_stocks")
+
+for(i in 1 :no)
+{
+  try( if(1==1)
+  { 
+    data <- data.frame()
+    stock=as.character(futures_list[i,4])
+    query<-paste("SELECT DISTINCT TIMESTAMP FROM " , stock," ORDER by TIMESTAMP ASC" ,sep="")
+    tab<-(dbGetQuery(connection.TWS_PERMIN_stocks,query)) ## implements the query
+    cat("TWS_PERMIN_stocks" ,timestamp(),"TwsPerminStocksValidateForTime",paste("Read", stock ,sep=" "),"\n",file = connection.log, sep = ",")
+    dates<-(tab[1])
+    dates.vector<-as.vector(dates[,1])
+    date.time<-timeDate(dates.vector, format = "%Y-%m-%d %H:%M:%S")
+    dif.in.time<-diff(date.time)
+    break.ind <-which(dif.in.time!=1)
+    length.break<-as.character(dif.in.time[break.ind])
+    length.break2<-as.numeric(length.break)
+    type<-ifelse(length.break2>1000,0,1)
+    differencedateend<-as.character(date.time[break.ind+1])
+    differencedatestart<-as.character(date.time[break.ind])
+    start.date<- (substring(differencedatestart,1,10))
+    end.date<- (substring(differencedateend,1,10))
+    start.time<-(substring(differencedatestart,12,19))
+    end.time<-substring(differencedateend,12,19)
+    tablename=stock
+    d<-cbind(start.date,start.time,end.date,end.time, length.break2,type)
+    data<-as.data.frame(d)
+    names(data)<-c("START_DATE","START_TIME","END_DATE","END_TIME","DIFFERENCE","BREAK_TYPE")
+    str<-ifelse( nrow(data)>0 , "Added" , "Not Added")
+    cat("VALIDATION_TWS_PERMIN_stocks" ,timestamp(),"TwsPerminStocksValidateForTime",paste(str, tablename ,sep=" "),"\n",file = connection.log, sep = ",")
+    ifelse(dbExistsTable(connection.VALIDATION_TWS_PERMIN_stocks, tablename),dbWriteTable(connection.VALIDATION_TWS_PERMIN_stocks, name =tablename, value=data, append = T),dbWriteTable(connection.VALIDATION_TWS_PERMIN_stocks, name = tablename, value=data))
+  } 
+  ,silent=TRUE)
+}
+close(connection.log)
+dbDisconnect(connection.TWS_PERMIN_stocks)
+dbDisconnect(connection.VALIDATION_TWS_PERMIN_stocks)
+}
