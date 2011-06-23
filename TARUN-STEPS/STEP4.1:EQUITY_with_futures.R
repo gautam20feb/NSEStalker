f4a5dd0ee448cc14c5643fcd0279b8c058c6892d
Tarun Jain <tarunjain15@gmail.com>
2011-06-23 14:22:43 +0800

    TARUN-STEPS/STEP4.1:EQUITY_with_futures.R

--- TARUN-STEPS/STEP4.1:EQUITY_with_futures.R
+++ TARUN-STEPS/STEP4.1:EQUITY_with_futures.R
@@ -1,17 +1,16 @@
-library(lattice)
-library(fBasics)
-library(fImport)
-library(RMySQL)
+library (lattice)
+library (fBasics)
+library (fImport)
+library (RMySQL)
 m <- dbDriver("MySQL", max.con = 100)
-con <- dbConnect(m, user="root", password = "intern123", host = "localhost", dbname="NSE derivedDB")
-con1 <- dbConnect(m, user="root", password = "intern123", host = "localhost", dbname="NSE rawdatabase")
-st <-function(start,end)
-{
-  stock<-read.csv("STK.csv")
-  strStk=""
-  sapply(stock[,2],function(x) strStk<<-paste(strStk,",'",x,"'",sep=""))
-  strStk<-substr(strStk,2,nchar(strStk))
-  query<-paste("SELECT SYMBOL,CLOSE,PREVCLOSE,TIMESTAMP FROM equity WHERE TIMESTAMP BETWEEN '",start,"' AND '",end,"' AND SYMBOL IN","(",strStk,")"," AND SERIES ='EQ' ORDER BY SYMBOL,TIMESTAMP",sep="")
-  tab<-dbGetQuery(con1,query)
-  dbWriteTable(con, name = "equity_with_futures", value=tab,append=T)
+con.derived.db <- dbConnect(m, user="root", password = "intern123", host = "localhost", dbname="NSE derivedDB")
+con.rawdatabase <- dbConnect(m, user="root", password = "intern123", host = "localhost", dbname="NSE rawdatabase")
+CreatesEquityWithFutures <- function (start.date, end.date){
+  list.of.stocks <- read.csv ("STK.csv")
+  string.stocks = ""
+  sapply (list.of.stocks[,2], function (x) string.stocks <<- paste (string.stocks, ",'", x, "'", sep=""))
+  string.stocks <- substr (string.stocks, 2, nchar (string.stocks))
+  query <- paste("SELECT SYMBOL, CLOSE, PREVCLOSE, TIMESTAMP FROM equity WHERE TIMESTAMP BETWEEN '", start.date, "' AND '", end.date, "' AND SYMBOL IN", "(", string.stocks, ")", " AND SERIES ='EQ' ORDER BY SYMBOL, TIMESTAMP", sep="")
+  tab <- dbGetQuery (con.rawdatabase, query)
+  dbWriteTable (con.derived.db, name = "equity_with_futures", value = tab, append = T)
 }
--- TARUN-STEPS/STEP4.2: FUTURE_with_oneExpiry.R
+++ TARUN-STEPS/STEP4.2: FUTURE_with_oneExpiry.R
@@ -1,13 +1,15 @@
-library(lattice)
-library(fBasics)
-library(fImport)
-library(RMySQL)
-m <- dbDriver("MySQL", max.con = 100)
-con <- dbConnect(m, user="root", password = "intern123", host = "localhost", dbname="NSE derivedDB")
-con1 <- dbConnect(m, user="root", password = "intern123", host = "localhost", dbname="NSE rawdatabase")
-st <-function(start,end)
-{
-  query<-paste("SELECT INSTRUMENT, SYMBOL,  min(EXPIRY_DT),OPEN,HIGH,LOW,CLOSE,SETTLE_PR,CONTRACTS,VAL_INLAKH, OPEN_INT,CHG_IN_OI, TIMESTAMP FROM future WHERE TIMESTAMP BETWEEN '",start,"' AND '",end,"' GROUP BY TIMESTAMP,SYMBOL ORDER BY SYMBOL,TIMESTAMP",sep="")
-  tab<-dbGetQuery(con1,query)
-  dbWriteTable(con, name = "future_earliest_expiry_dt", value=tab,append=T)
+library (lattice)
+library (fBasics)
+library (fImport)
+library (RMySQL)
+m <- dbDriver ("MySQL", max.con = 100)
+con.derived <- dbConnect (m, user="root", password = "intern123", host = "localhost", dbname="NSE derivedDB")
+con.rawdatabase <- dbConnect (m, user="root", password = "intern123", host = "localhost", dbname="NSE rawdatabase")
+CreatesFuturesWithNearestExpiry <- function (start.date, end.date){
+  query <- paste ("SELECT INSTRUMENT, SYMBOL, min(EXPIRY_DT), OPEN, HIGH, LOW, CLOSE, SETTLE_PR, CONTRACTS, 
+                  VAL_INLAKH, OPEN_INT, CHG_IN_OI, TIMESTAMP FROM future WHERE TIMESTAMP BETWEEN '",
+                  start.date, "' AND '", end.date, "' GROUP BY TIMESTAMP, SYMBOL ORDER BY SYMBOL, TIMESTAMP",
+                  sep="")
+  min.expiry.table <- dbGetQuery (con.rawdatabase, query)
+  dbWriteTable (con.derived, name = "future_earliest_expiry_dt", value = min.expiry.table, append=T)
 }
