0b60cd32e5e91b91e5504f2e7a93063175d4d1d8
Rajesh Kella <krajesh1990@gmail.com>
2011-06-24 15:12:35 +0530

    modified .R

--- Stk_yahoo_data_to_database.R
+++ Stk_yahoo_data_to_database.R
@@ -2,6 +2,8 @@
 library (fBasics)    
 library (fImport)
 library (RMySQL)
+library (timeSeries)
+library (timeDate)
 
 source("../lib/read_machines.R")
 source("../lib/create_connection.R")
@@ -15,22 +17,26 @@ user.name = "intern@Ophelia"
 ### The default user name is intern
 ){
   machines = GenerateMachimesDataframe(config_path)
-  con <- CreateConnection (user.name , machines ,"YAHOO_OHLC_stocks") # Setting up connection. Create yahoo_data in phpmyadmin before running this
+  connection.YAHOO_OHLC_stocks <- CreateConnection (user.name , machines ,"YAHOO_OHLC_stocks") # Setting up connection. Create yahoo_data in phpmyadmin before running this
   
-  futures_list <- read.csv("../data/futures_list.csv", sep=";") # imports the file containing symbols compatible with yahoo
+  futures_list <- read.csv("../data/futures_list.csv", sep=",") # imports the file containing symbols compatible with yahoo
+  symbols <- futures_list
   
   for(i in 1 : nrow (symbols))
     try({  # Skips the iteration if it doesn't run
-      series.data1 <- yahooSeries (as.character (symbols[i,2]), from = "2010-07-01", to = Sys.timeDate())
-      # Creating a vector of yahoo series from given date to system date
+      series.data1 <- yahooSeries (as.character (symbols[i,6]), from = "2010-07-01", to = Sys.timeDate())
+      ### Creating a vector of yahoo series from given date to system date
       series.data<-as.data.frame(series.data1)  # Converting the vector into data frame
-      colnames(series.data) <- paste(c("Open", "High", "Low", "Close", "Volume", "A.Close"),sep = ",")
-      # Creating header from series.data  
-      ifelse(dbExistsTable(con,as.character(symbols[i,1])),dbWriteTable(con,symbols[i,1], value=series.data, append = T),dbWriteTable(con,as.character(symbols[i,1]), value=series.data))
-      # If file doesn't exist, creates and writes to it. Else it appends
-      cat (as.character (timestamp()), "adding to the yahoo_data table", i, as.character(symbols [i,1]), "\n", file = connection.log, sep = ",")
-      # Tracking the running status of the program
-    }, silent = TRUE)  # remains silent if some iteration doesn't run
-  list <- dbListTables (con)  # Lists all the table names in yahoo_data
-  diff <- setdiff(symbols [ ,1], list)  # Shows all those in yahoo symbols list that are not in the vector list
-}
+      names(series.data) <- paste(c("OPEN", "HIGH", "LOW", "CLOSE", "VOLUME", "ADJ_CLOSE"),sep = ",")
+      ### Creating header from series.data  
+      ifelse(dbExistsTable(connection.YAHOO_OHLC_stocks,as.character(symbols[i,4])),dbWriteTable(connection.YAHOO_OHLC_stocks,symbols[i,4], value=series.data, append = T),dbWriteTable(connection.YAHOO_OHLC_stocks,as.character(symbols[i,4]), value=series.data))
+      ### If file doesn't exist, creates and writes to it. Else it appends
+      cat ("YAHOO_OHLC_stocks",timestamp(), "AddYahooToDatabse","adding to the yahoo_data table", i, as.character(symbols [i,1]), "\n", file = connection.log, sep = ",")
+      ### Tracking the running status of the program
+    }, silent = TRUE)  ##<< remains silent if some iteration doesn't run
+  list <- dbListTables (connection.YAHOO_OHLC_stocks)  ##<< Lists all the table names in yahoo_data
+  diff <- setdiff(symbols [ ,4], list)  ##<< Shows all those in yahoo symbols list that are not in the vector list
+  print(list)
+  print(diff)
+  dbDisconnect(connection.YAHOO_OHLC_stocks)
+  }
