cd7817f6c8ca1603c12d450f2f204fb2ce7a4ab7
Gautam Garg <gautam20feb@gmail.com>
2011-06-24 16:15:59 +0800

    deleted the old files

--- TWS/Add_symbols_to_db.R
+++
@@ -1,7 +0,0 @@
-m <- dbDriver("MySQL", max.con = 25)
-conn <- dbConnect(m, user="root", password = "intern123", host = "localhost", dbname= "Tws_daily_data") ## sets the connection with the required database
-ss="Stocks"
-stk<-read.table("STK.with.Sym.csv", sep=",", header=T)
-
- names(stk)<-c("ID" ,"NSE_NAME","TWS_SYMBOL","NAME_USED")
-   ifelse(dbExistsTable(conn, ss),dbWriteTable(conn, name =ss, value=stk, append = T),dbWriteTable(conn, name = ss, value=stk))
