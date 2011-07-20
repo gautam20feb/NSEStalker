0f78f15eb4ee23d060d4530b7e0e65a81ea930ec
Gautam Garg <gautam20feb@gmail.com>
2011-07-20 19:08:23 +0800

    modified

--- create_metadata.R
+++ create_metadata.R
@@ -5,27 +5,29 @@ library(RMySQL)
 library(XML)
 library(gregmisc)
 
+
 source("../lib/read_machines.R")
 source("../lib/create_connection.R")
 connection.log <- file(paste("../log/" ,as.character(timestamp()),"log.csv",sep="") , "w")  ##<< For logging the files written to database
 
 config_path <- "../config/"
+user.name = "intern@Ophelia"
+machines=GenerateMachimesDataframe(config_path)
 
 CreateMetadataDatabase <- function(
-user.name = "intern@Ophelia")
+user.name = "intern@Ophelia"
+)
 {
+  conn1 <- CreateConnection(user.name , machines ,"METADATA_DATABASE")
+connNos  <- CreateConnection(user.name , machines ,"NSE_OHLC_stocks")
+connNof <- CreateConnection(user.name , machines ,"NSE_OHLC_futures")
+connTos<- CreateConnection(user.name , machines ,"TWS_OHLC_stocks")
+connTpf <- CreateConnection(user.name , machines ,"TWS_PERMIN_futures")
+connTps <- CreateConnection(user.name , machines ,"TWS_PERMIN_stocks")
+
+  
  stocks<-read.csv("../data/futures_list.csv", sep=",", header=T)
- 
-  machines=GenerateMachimesDataframe(config_path)
-  connection.METADATA_DATABASE <- CreateConnection(user.name , machines ,"METADATA_DATABASE")
-  connNof <- CreateConnection(user.name , machines ,"NSE_OHLC_futures")
-  connNos <- CreateConnection(user.name , machines ,"NSE_OHLC_stocks")
-  connTos <- CreateConnection(user.name , machines ,"TWS_OHLC_stocks")
-  connTpf <- CreateConnection(user.name , machines ,"TWS_PERMIN_futures")
-  connTps <- CreateConnection(user.name , machines ,"TWS_PERMIN_stocks")
-  conn <- connection.METADATA_DATABASE
- 
- 
+
   noNos=c()
   noNof=c()
   noTos=c()
@@ -35,7 +37,7 @@ user.name = "intern@Ophelia")
   
   for(i in 1:nrow(stocks))
   {
-  curr.stk<-trim(as.character(stocks[i,4]))
+  curr.stk<-trim(as.character(stocks$TABLE_NAME[i]))
   query<- paste("SELECT max(TIMESTAMP) from ", curr.stk  ,sep="")
   query2<- paste("SELECT MIN(VERSION) from ", curr.stk  ,sep="")
   curr.time<- as.character(Sys.time())
@@ -53,7 +55,7 @@ user.name = "intern@Ophelia")
   data<-cbind(curr.stk,endNos, curr.time,version)
   names(data)<- c("TABLE_NAME","END_DATE","LAST_UPDATED_DATE_TIME","VERSION")
   tablename="NSE_OHLC_stocks"
-  ifelse(dbExistsTable(conn, tablename),dbWriteTable(conn, name =tablename, value=data, append = T),dbWriteTable(conn, name = tablename, value=data))
+  ifelse(dbExistsTable(conn1, tablename),dbWriteTable(conn1, name =tablename, value=data, append = T),dbWriteTable(conn1, name = tablename, value=data))
   cat("METADATA_DATABASE", timestamp(),"CreateMetadataDatabase", paste("Created metadata for",curr.stk,"of",tablename,sep=" "),"\n", file = connection.log)  
   }
   else noNos<-c(noNos,curr.stk)
@@ -64,7 +66,7 @@ user.name = "intern@Ophelia")
   data<-cbind(curr.stk,endNof, curr.time,version)
   names(data)<- c("TABLE_NAME","END_DATE","LAST_UPDATED_DATE_TIME","VERSION")
   tablename="NSE_OHLC_futures"
-  ifelse(dbExistsTable(conn, tablename),dbWriteTable(conn, name =tablename, value=data, append = T),dbWriteTable(conn, name = tablename, value=data))
+  ifelse(dbExistsTable(conn1, tablename),dbWriteTable(conn1, name =tablename, value=data, append = T),dbWriteTable(conn1, name = tablename, value=data))
   cat("METADATA_DATABASE", timestamp(),"CreateMetadataDatabase", paste("Created metadata for",curr.stk,"of",tablename,sep=" "),"\n", file = connection.log)  
   }
   else noNof<-c(noNof,curr.stk)  
@@ -76,7 +78,7 @@ user.name = "intern@Ophelia")
   data<-cbind(curr.stk,endTos, curr.time, version)
   names(data)<- c("TABLE_NAME","END_DATE","LAST_UPDATED_DATE_TIME","VERSION")
   tablename="TWS_OHLC_stocks"
-  ifelse(dbExistsTable(conn, tablename),dbWriteTable(conn, name =tablename, value=data, append = T),dbWriteTable(conn, name = tablename, value=data))
+  ifelse(dbExistsTable(conn1, tablename),dbWriteTable(conn1, name =tablename, value=data, append = T),dbWriteTable(conn1, name = tablename, value=data))
   cat("METADATA_DATABASE", timestamp(),"CreateMetadataDatabase", paste("Created metadata for",curr.stk,"of",tablename,sep=" "),"\n", file = connection.log)  
 
   }
@@ -91,7 +93,7 @@ user.name = "intern@Ophelia")
 #   data<-cbind(curr.stk,endTof, curr.time,version)
 #   names(data)<- c("TABLE_NAME","END_DATE","LAST_UPDATED_DATE_TIME","VERSION")
 #   tablename="TWS_OHLC_futures"
-#   ifelse(dbExistsTable(conn, tablename),dbWriteTable(conn, name =tablename, value=data, append = T),dbWriteTable(conn, name = tablename, value=data))
+#   ifelse(dbExistsTable(conn1, tablename),dbWriteTable(conn1, name =tablename, value=data, append = T),dbWriteTable(conn1, name = tablename, value=data))
 #   }
 #   else noTof<-c(noTof,curr.stk)
   if(
@@ -104,7 +106,7 @@ user.name = "intern@Ophelia")
   data<-cbind(curr.stk,endTps, curr.time,version)
   names(data)<- c("TABLE_NAME","END_DATE","LAST_UPDATED_DATE_TIME","VERSION")
   tablename="TWS_PERMIN_stocks"
-  ifelse(dbExistsTable(conn, tablename),dbWriteTable(conn, name =tablename, value=data, append = T),dbWriteTable(conn, name = tablename, value=data))
+  ifelse(dbExistsTable(conn1, tablename),dbWriteTable(conn1, name =tablename, value=data, append = T),dbWriteTable(conn1, name = tablename, value=data))
   cat("METADATA_DATABASE", timestamp(),"CreateMetadataDatabase", paste("Created metadata for",curr.stk,"of",tablename,sep=" "),"\n", file = connection.log)  
   
   }
@@ -117,7 +119,7 @@ user.name = "intern@Ophelia")
   data<-cbind(curr.stk,endTpf, curr.time,version)
   names(data)<- c("TABLE_NAME","END_DATE","LAST_UPDATED_DATE_TIME","VERSION")
   tablename="TWS_PERMIN_futures"
-  ifelse(dbExistsTable(conn, tablename),dbWriteTable(conn, name =tablename, value=data, append = T),dbWriteTable(conn, name = tablename, value=data))
+  ifelse(dbExistsTable(conn1, tablename),dbWriteTable(conn1, name =tablename, value=data, append = T),dbWriteTable(conn1, name = tablename, value=data))
   cat("METADATA_DATABASE", timestamp(),"CreateMetadataDatabase", paste("Created metadata for",curr.stk,"of",tablename,sep=" "),"\n", file = connection.log)  
 
   }
